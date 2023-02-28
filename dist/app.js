bd63e3f7cfbc46abb4c77c563c283e3d2b0e6423
Manirakiza Blaise <manirakizablaise@gmail.com>
2023-02-28 18:50:00 +0200

    Les interfaces

--- dist/app.js
+++ dist/app.js
@@ -1,38 +1,19 @@
 "use strict";
+window.GoogleAnalytics;
+//on peut aussi les implementes
+class Triangle {
+    constructor() {
+        this.x = 3;
+        this.y = 4;
+        this.z = 2;
+    }
+    get() {
+        return this.x;
+    }
+}
+function draw(p) { }
 /**
- * vue que la methode reverse modifie le tableau original
- * et si on veut qu'il soie en lecture seul :On utilise readOnly
- * et si on veut que le retour soit en lecture seul: on utilise readonly
- * @param arr T
- * @returns T
- */
-function reverse(arr) {
-    //on cree un nouveau tableau puis utilise le renverse
-    return [...arr].reverse();
-}
-class User {
-    constructor(firstname) {
-        this.firstname = firstname;
-    }
-}
-class Collection {
-    constructor(items) {
-        this.items = items;
-    }
-    /**
-     * On peut retourne l'instance actuelle
-     */
-    add(item) {
-        this.items.push(item);
-        return this;
-    }
-    first() {
-        var _a;
-        return (_a = this.items[0]) !== null && _a !== void 0 ? _a : null;
-    }
-}
-const a = new Collection([
-    new User("bob"),
-    new User("John")
-]);
-console.log(a.first());
+ * vue que draw attend comme argument de type Point
+ * les classes qui implementes cette interface seront le bienvenue
+ * */
+draw(new Triangle());
--- src/app.ts
+++ src/app.ts
@@ -1,38 +1,34 @@
+//Les interfaces restent ouverts
+interface Point {
+  x: number;
+  y: number;
+  get(): number;
+}
+
+interface Point {
+  z: number;
+}
+
+//autre exemple => modifie l'interface de window
+interface Window {
+  GoogleAnalytics: string;
+}
+window.GoogleAnalytics;
+
+//on peut aussi les implementes
+class Triangle implements Point {
+  x = 3;
+  y = 4;
+  z = 2;
+  get(): number {
+    return this.x;
+  }
+}
+
+function draw(p: Point) {}
+
 /**
- * vue que la methode reverse modifie le tableau original
- * et si on veut qu'il soie en lecture seul :On utilise readOnly
- * et si on veut que le retour soit en lecture seul: on utilise readonly
- * @param arr T
- * @returns T
- */
-
-function reverse<T>(arr: readonly T[]): T[] {
-  //on cree un nouveau tableau puis utilise le renverse
-  return [...arr].reverse();
-}
-class User {
-  constructor(
-    private firstname:string
-  ){}
-}
-class Collection<T> {
-  constructor(private items: T[]) {}
-
-  /**
-   * On peut retourne l'instance actuelle
-   */
-  add(item: T):this{
-    this.items.push(item);
-    return this;
-  }
-
-  first(): T | null {
-    return this.items[0] ?? null;
-  }
-}
-
-const a = new Collection<User>([
-  new User("bob"),
-  new User("John")
-]);
-console.log(a.first());
+ * vue que draw attend comme argument de type Point
+ * les classes qui implementes cette interface seront le bienvenue
+ * */
+draw(new Triangle());
