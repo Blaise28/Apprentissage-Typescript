2b4620794a5832912595862938998e1312457f26
Manirakiza Blaise <manirakizablaise@gmail.com>
2023-02-28 09:47:08 +0200

    Narrowing avec Typescript

--- dist/app.js
+++ dist/app.js
@@ -1,16 +1,44 @@
 "use strict";
-const a = 2;
-const b = true;
-const c = ["hey", "hi", "ds"];
-const user = {
-    firstName: "John",
-    lastName: "Rambo",
-};
-let date = new Date();
 //La forme de cette fonction
 const cb = (e) => { };
 function printId(id) {
-    console.log(id.toString);
+    if (typeof id === "number") {
+        // il sera capable de detecter que id est de type number
+        console.log(id * 3);
+    }
+    else {
+        //et ici de type string
+        console.log(id.toUpperCase);
+    }
+}
+function test(a, b) {
+    if (a === b) {
+        /**
+         * dans ce cas il sera capable de dire que a et b seront des string
+         * car ces dans ces deux cas possible
+         * */
+    }
+}
+function exemple(a) {
+    if (isDate(a)) {
+        /**
+         * a sera de type Date
+         */
+        a;
+    }
+    if ("value" in a) {
+        /**
+         * a est de type HTMLInputElement car dans ces objets
+         * value existe dans HtmlInputElement
+         */
+    }
+}
+function isDate(a) {
+    /**
+     * vue qu'on sait precisement le type qu'il va retourne
+     * on peut dire que notre fonction retourne vrai si c'est un Date
+     */
+    return a instanceof Date;
 }
 const compteur = document.querySelector("#compteur");
 let i = 0;
--- src/app.ts
+++ src/app.ts
@@ -1,31 +1,59 @@
-const a: number = 2;
-const b: boolean = true;
-const c: string[] = ["hey", "hi", "ds"];
-const user: { firstName: string; lastName?: string } = {
-  firstName: "John",
-  lastName: "Rambo",
-};
-let date: Date = new Date();
-
 //La forme de cette fonction
 const cb: (e: Event) => void = (e: Event): void => {};
 
 function printId(id: number | string): void {
-  console.log(id.toString);
+  if (typeof id === "number") {
+    // il sera capable de detecter que id est de type number
+    console.log(id * 3);
+  } else {
+    //et ici de type string
+    console.log(id.toUpperCase);
+  }
+}
+function test(a: string | boolean, b: string | number) {
+  if (a === b) {
+    /**
+     * dans ce cas il sera capable de dire que a et b seront des string
+     * car ces dans ces deux cas possible
+     * */
+  }
 }
 
-const compteur = document.querySelector("#compteur") ;
+function exemple(a: HTMLInputElement | MouseEvent | Date) {
+  if(isDate(a)){
+    /**
+     * a sera de type Date
+     */
+    a
+  }
+  if ("value" in a) {
+    /**
+     * a est de type HTMLInputElement car dans ces objets
+     * value existe dans HtmlInputElement
+     */
+  }
+}
+
+function isDate(a: any): a is Date{
+/**
+ * vue qu'on sait precisement le type qu'il va retourne
+ * on peut dire que notre fonction retourne vrai si c'est un Date
+ */
+  return a instanceof Date;
+}
+
+const compteur = document.querySelector("#compteur");
 let i = 0;
 
-const increment: (e:Event)=>void = (e:Event)=>{
-    i++;
-    const span = compteur?.querySelector("span");
-    if(span){
-        span.textContent = i.toString();
-    }
-}
+const increment: (e: Event) => void = (e: Event) => {
+  i++;
+  const span = compteur?.querySelector("span");
+  if (span) {
+    span.textContent = i.toString();
+  }
+};
 
 /**
  * ? veut dire n'utilise compteur que s'il existe
  */
-compteur?.addEventListener("click",increment);
+compteur?.addEventListener("click", increment);
